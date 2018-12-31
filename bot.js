3ae822a016cd9ee2e7925900213355a6a7116372
YASSIN974 <44032576+YASSIN974@users.noreply.github.com>
2018-12-31 17:13:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -816,11 +816,33 @@ ava.src = buf;
                         ctx.clip();
                         ctx.drawImage(ava, 116, 82, 72, 72);
  
-message.channel.startTyping()
-message.channel.sendFile(canvas.toBuffer())
-message.channel.stopTyping()
+  
+ //.then(message =>{message.delete(2000)})
+ 
 })
 })
+}
+});
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== "332713449215754242") return;
+
+if (message.content.startsWith(prefix + 'setstream')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/darkknite55");
+     console.log('test' + argresult);
+    message.channel.sendMessage(`Streaming: **${argresult}`)
+}
+
+if (message.content.startsWith(prefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.sendMessage(`Username Changed To **${argresult}**`)
+  return message.reply("You Can change the username 2 times per hour");
+}
+if (message.content.startsWith(prefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
 }
 });
 client.login(process.env.BOT_TOKEN);
